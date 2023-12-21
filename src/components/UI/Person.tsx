9cb27342f0b6f45626c93c4bf8f31ae17cd479fa
Janetta Kattoeva <jkattoeva@gmail.com>
2023-12-22 00:29:03 +0600

    create ui component

--- src/components/UI/Person.tsx
+++ src/components/UI/Person.tsx
@@ -1,7 +1,13 @@
+import cls from "@/styles/Person.module.scss";
+
 export default function Person() {
   return (
     <>
-      <div></div>
+      <div className={cls.person}>
+        <div className={cls.image}>
+          <img src="" alt="janet picture" />
+        </div>
+      </div>
     </>
   );
 }
