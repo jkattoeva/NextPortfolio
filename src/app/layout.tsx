9224614f5c011674c9ba0b7c14865166bd87b2f4
Janetta Kattoeva <jkattoeva@gmail.com>
2023-12-25 00:12:42 +0600

    change the  bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import RootLayoutClient from "./layout.uc";
 import "./globals.css";
-import Header from "@/components/header/Header";
 
 interface Props {
   children: React.ReactNode;
